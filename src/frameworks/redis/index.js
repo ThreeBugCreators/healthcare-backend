7abb80634ae89ee297ab00272d3b904bd3c2e065
thinh.le <roshan1234.lol@gmail.com>
2022-12-13 22:07:17 +0700

    Hotfix #2

--- src/frameworks/redis/index.js
+++ src/frameworks/redis/index.js
@@ -4,6 +4,7 @@ import { redisConfiguration } from '../../configs/index.js';
 
 const setupRedis = async () => {
     const redis = redisConnection.createRedisClient(redisConfiguration);
+    redis.on('error', err => console.error('client err', err));
     await redis.connect();
     return promisify(redis);
 };
